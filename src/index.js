3e013241fe389e3ca128f64d8c72d9bb1110fb6f
500inthegraveyard <hoostasr@gmail.com>
2020-08-12 16:56:11 -0500

    push without app.listen in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-app.listen(process.env.PORT || 3000);
 
 ReactDOM.render(<App />, document.getElementById("root"));
